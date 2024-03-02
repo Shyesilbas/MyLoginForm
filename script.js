b981769adc26efb48227845d936e340a4b35be4f
Serhathy <shyesilbas@gmail.com>
2024-03-02 16:06:54 +0300

    minor update

---
+++ script.js
@@ -0,0 +1,78 @@
+function RefreshPage(){
+    window.location.reload();
+}
+function GetTime(){
+    let anlıkTarih = new Date();
+    let gün = anlıkTarih.getDate();
+    let ay = anlıkTarih.getMonth()+1;
+    let yıl = anlıkTarih.getFullYear();
+    let saat =anlıkTarih.getHours();
+    let dakika = anlıkTarih.getMinutes();
+    let saniye = anlıkTarih.getSeconds();
+     
+    ay = ("0"+ay).slice(-2);
+    gün = ("0"+gün).slice(-2);
+    saat =("0"+saat).slice(-2);
+    dakika=("0"+dakika).slice(-2); 
+    saniye = ("0" + saniye).slice(-2);
+    let AnlıkSaatDiv = document.getElementById("Time-container");
+    AnlıkSaatDiv.textContent = `${gün}.${ay}.${yıl} - ${saat}:${dakika}:${saniye}`;
+}
+setInterval(GetTime,1000);
+GetTime();
+
+
+var passwordField = document.getElementById("password");
+var toggleIcon = document.getElementById("togglePassword");
+var isPasswordVisible = false;
+function togglePassword() {
+isPasswordVisible = !isPasswordVisible
+
+if (isPasswordVisible) {
+    passwordField.type = "text";
+    toggleIcon.textContent = "👀";
+} else {
+    passwordField.type = "password";
+    toggleIcon.textContent = "👁️";
+}
+}
+
+function togglePassword2() {
+var passwordInput = document.getElementById("password2");
+var toggleIcon = document.getElementById("togglePassword");
+
+if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    toggleIcon.textContent = "👀";
+} else {
+    passwordInput.type = "password";
+    toggleIcon.textContent = "👁️";
+}
+}
+
+function SwitchForm() {
+var registerContainer = document.getElementById("Register-container");
+var loginContainer = document.getElementById("Login-container");
+
+if (registerContainer.style.display === "block" || registerContainer.style.display === "") {
+    registerContainer.style.display = "none";
+    loginContainer.style.display = "block";
+} else {
+    registerContainer.style.display = "block";
+    loginContainer.style.display = "none";
+}
+}
+function strongPassword(input){
+var strength = input.value;
+var strongpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,101}$/;
+
+if(strongpassword.test(strength)){
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+}
+else{
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+}
+
+}
